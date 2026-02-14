f30dc58c35edaada162f398bae6bf4f7b9d0c8d7
cdtpalers <cdtpalero@gmail.com>
2026-02-14 12:04:34 +0800

    Update song cover URLs

--- constants.ts
+++ constants.ts
@@ -75,7 +75,7 @@ export const PLAYLIST: Song[] = [
     id: '1',
     title: "Lifetime",
     artist: "Ben&Ben",
-    coverUrl: "https://scontent.fmnl14-2.fna.fbcdn.net/v/t39.30808-6/467267536_10227814890625316_3240943355742182016_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGxD1HhGCGKPJfNC8_TDOvpvf6NB3aij4m9_o0HdqKPidUJCYVkxKL8p2Mrghp-m2sJFUiXtnx_xh_YHGi1eER3&_nc_ohc=t-uHVBCwqqgQ7kNvwFUaeCN&_nc_oc=Adlul5gE8pMZSWLnGczhsNyifxOTDHGqnVaaxibKwHtunGmEos25UClfc2yM1cmB-j0&_nc_zt=23&_nc_ht=scontent.fmnl14-2.fna&_nc_gid=uzqEdn5UFeLyHq0V_vWfhA&oh=00_Afvbd4HF5CK9zKUi-9D3J8MRHTmymBTNL_3YXiGQ9YxIxA&oe=69951AC7", // Placeholder image
+    coverUrl: "https://scontent.fmnl14-2.fna.fbcdn.net/v/t39.30808-6/634884928_1631474138279607_1600884015483074888_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=V_iacsArDfAQ7kNvwEIvbSv&_nc_oc=AdkixepC4TJFWM71fc3tcl51Cbv5k003eWXabxAslhKng2hAra6hPoTkq9MTBb9mBA4&_nc_zt=23&_nc_ht=scontent.fmnl14-2.fna&_nc_gid=qfSUUWw5tVye4spZ62JuMQ&oh=00_AfvSBwymabfMXJHO5tTfOag7CC68fjfoRyv1uMYRUkBG_g&oe=6995B327", // Placeholder image
     audioUrl: "https://audio.jukehost.co.uk/7zXFeS7VwVjaccjSZcboIGdScxTCVJWg", // Example royalty-free MP3
     note: "always remember that i will always choose you",
   },
@@ -83,7 +83,7 @@ export const PLAYLIST: Song[] = [
     id: '2',
     title: "Make It With You",
     artist: "Ben&Ben",
-    coverUrl: "https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-6/633242236_25718909007731967_1390839016425213415_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGmU-IFfsJa1ESJEGgDjWt4KkmEi7ekaAkqSYSLt6RoCf8EVk5W4eXeyI7mrPNaqm3_QLaOQEcgWZrkdyAHktCr&_nc_ohc=UVofPmKnvb0Q7kNvwGqmlGg&_nc_oc=AdmkfzpBOAxt03vlFH_PutoXHISRldkCLM1ZYYTQZFAPzD7a4TMDVon_N0krjSvIhH0&_nc_zt=23&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=wvj9a8GJVQjvAO_iSU9cIw&oh=00_Afv8oElmytsbOfZ4Ick8KXQklrIE5e2BtfTGpkEpEzuedg&oe=69950079",
+    coverUrl: "https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-6/633018734_1631474121612942_5803761759293836611_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=N8o_92dCKGAQ7kNvwFzBs_n&_nc_oc=AdmQzpYo6Fy2IYqpbWHcXswWHvuXYXjF1kxy-uJOdt8326HlzZaHDVxBlWvTAPC-64I&_nc_zt=23&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=rVeud19tmUAKiKUcP7qXfA&oh=00_AftTvAX0dB1co6Suddj54vbJYZf1iYj47RR0U3M2LRsFUw&oe=6995C048",
     audioUrl: "https://audio.jukehost.co.uk/LcQloUqivH2paNsJd8qoaLDPG4G0Gkwf",
     note: "malayo man tayo sa isat isa pero papatunayan ko na ikaw at ikaw lang ang iibigin ko",
   },
@@ -91,7 +91,7 @@ export const PLAYLIST: Song[] = [
     id: '3',
     title: "Araw Araw",
     artist: "Ben&Ben",
-    coverUrl: "https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-6/495349435_4122846914703048_1886679763937689094_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeH-CdZzzz58ZMKN_g1ZaApPWIRhdg48XVxYhGF2DjxdXPfDVE1U7Wlmzz7xlxZfRSOLlheD4Kv-B5WvEV1KalwT&_nc_ohc=RTaXL0EPwlcQ7kNvwEfdMLF&_nc_oc=AdnAtWWgHKOFo-WmA5--slY6DDUnuRXfG2T1MpH18_E8BAweqHFUdp1Yo2lq3dxs0Kw&_nc_zt=23&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=sbfefZAuUtgEvXjQZRjDHA&oh=00_AfuQa2zJjrbkgaWVvJQzPT_-0VkZw6P9ynbR-_SaXXCAFA&oe=699518BC",
+    coverUrl: "https://scontent.fmnl14-2.fna.fbcdn.net/v/t39.30808-6/633957844_1631474124946275_4089246019893141067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=F0HTd1AbQDwQ7kNvwEwDj5O&_nc_oc=AdkizHvDsdTg_inezOUzipLcxG9PEfyaOqFxnXajSTTlwah8sQEdE0uMCNkR-fOMtIo&_nc_zt=23&_nc_ht=scontent.fmnl14-2.fna&_nc_gid=xDyyKPiuV4NdJjLBjyfH8g&oh=00_AfssWvzDgZXxcMRfjBS8FgMzHnQx5LKvUtuoYfw7AgC7JA&oe=6995A602",
     audioUrl: "https://audio.jukehost.co.uk/mcfXftJkKzhcTHS0Kc1GZStITAbK07XB",
     note: "pasensya na kung minsan nawawalan ako ng oras sayo pero tandaan mo na nandito ako para sayo",
   },
