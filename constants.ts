bf109d5d723faf00f7e759045968a771935ebafe
cdtpalers <cdtpalero@gmail.com>
2026-02-14 12:01:05 +0800

    Update love notes and playlist

--- constants.ts
+++ constants.ts
@@ -13,57 +13,57 @@ export const PARTNER_NAME = "Sophia"; // Change this to your partner's name
 export const LOVE_NOTES: LoveNote[] = [
   {
     id: '1',
-    content: "Your eyes literally makes me melt wabiii🫣",
+    content: "gobless love sa incoming final demo, kaya mo yan",
     color: 'bg-blue-100',
   },
   {
     id: '2',
-    content: "ingat ka sa LDC huhuh, I will miss u somuchyyyy",
+    content: "no words can explain how much i miss you",
     color: 'bg-indigo-100',
   },
   {
     id: '3',
-    content: "You are my best friend, my greatest support, and the love of my life.",
+    content: "love thank you always reminding me that i am worthy for what i have right now",
     color: 'bg-blue-50',
   },
   {
     id: '4',
-    content: "Never na mauulet yung sa last September monthsaryy😉",
+    content: "i promise that i will always stay loyal to you",
     color: 'bg-sky-50',
   },
   {
     id: '5',
-    content: "I can't wait to live with you soon when we get married. We stay grounded lang sa ating faith kay GOD wabi.",
+    content: " onti nalang love maabot mo yung pinapangarap mo, i'm always proud of u",
     color: 'bg-indigo-50',
   },
   {
     id: '6',
-    content: "let's see our dreams come true together babyyy",
+    content: " sorry kung lagi kitang natutulugan",
     color: 'bg-sky-50',
   },
   {
     id: '7',
-    content: "im sorry if naaaway kita, that just means nagtatampo akoo and i want lambing hehehe",
+    content: "patawarin mo kung minsan nappag initan kita ulo hehehe",
     color: 'bg-indigo-100',
   },
   {
     id: '8',
-    content: "i want hugsss na so tightt babyy",
+    content: "kung may mga problema ka man tandaan mo nandito lang ako lagi para sayo",
     color: 'bg-indigo-100',
   },
   {
     id: '9',
-    content: "its ur eyes that i want to see when i wake up love, ginaya ko lng from u🥰",
+    content: "sana ikaw na yung makakasama ko hanggang sa pagtanda",
     color: 'bg-sky-50',
   },
   {
     id: '10',
-    content: "sana di ka magsawa at ma-bored sken hanggang pagtanda😙",
+    content: "praying for more memories and achievement with u love",
     color: 'bg-sky-50',
   },
   {
     id: '11',
-    content: "You're best thing that ever happened to meeeee poo😘",
+    content: " sana tumaba ka pa lalo, jk",
     color: 'bg-sky-50',
   },
 ];
@@ -73,34 +73,27 @@ export const LOVE_NOTES: LoveNote[] = [
 export const PLAYLIST: Song[] = [
   {
     id: '1',
-    title: "Estranghero",
-    artist: "Cup of Joe",
+    title: "Lifetime",
+    artist: "Ben&Ben",
     coverUrl: "https://scontent.fmnl14-2.fna.fbcdn.net/v/t39.30808-6/467267536_10227814890625316_3240943355742182016_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeGxD1HhGCGKPJfNC8_TDOvpvf6NB3aij4m9_o0HdqKPidUJCYVkxKL8p2Mrghp-m2sJFUiXtnx_xh_YHGi1eER3&_nc_ohc=t-uHVBCwqqgQ7kNvwFUaeCN&_nc_oc=Adlul5gE8pMZSWLnGczhsNyifxOTDHGqnVaaxibKwHtunGmEos25UClfc2yM1cmB-j0&_nc_zt=23&_nc_ht=scontent.fmnl14-2.fna&_nc_gid=uzqEdn5UFeLyHq0V_vWfhA&oh=00_Afvbd4HF5CK9zKUi-9D3J8MRHTmymBTNL_3YXiGQ9YxIxA&oe=69951AC7", // Placeholder image
-    audioUrl: "https://audio.jukehost.co.uk/8Q4KCNIsMBlSPEJ0dLZEPAnzVjqyvMEv", // Example royalty-free MP3
-    note: "yung song mo for me🤗 hehe kilig somuch sa AFPCES",
+    audioUrl: "https://audio.jukehost.co.uk/7zXFeS7VwVjaccjSZcboIGdScxTCVJWg", // Example royalty-free MP3
+    note: "always remember that i will always choose you",
   },
   {
     id: '2',
-    title: "You Don't Know",
-    artist: "Nico Play",
+    title: "Make It With You",
+    artist: "Ben&Ben",
     coverUrl: "https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-6/633242236_25718909007731967_1390839016425213415_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGmU-IFfsJa1ESJEGgDjWt4KkmEi7ekaAkqSYSLt6RoCf8EVk5W4eXeyI7mrPNaqm3_QLaOQEcgWZrkdyAHktCr&_nc_ohc=UVofPmKnvb0Q7kNvwGqmlGg&_nc_oc=AdmkfzpBOAxt03vlFH_PutoXHISRldkCLM1ZYYTQZFAPzD7a4TMDVon_N0krjSvIhH0&_nc_zt=23&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=wvj9a8GJVQjvAO_iSU9cIw&oh=00_Afv8oElmytsbOfZ4Ick8KXQklrIE5e2BtfTGpkEpEzuedg&oe=69950079",
-    audioUrl: "https://audio.jukehost.co.uk/DFLVwFqB7FTaRKo0NZNAH1hS82YJGQc8",
-    note: "You don't know How I long for you, How it keeps me awake, You don't know All that I would do, The vows I would make, Married life, A family, and a wife",
+    audioUrl: "https://audio.jukehost.co.uk/LcQloUqivH2paNsJd8qoaLDPG4G0Gkwf",
+    note: "malayo man tayo sa isat isa pero papatunayan ko na ikaw at ikaw lang ang iibigin ko",
   },
   {
     id: '3',
-    title: "Tulad Mo",
-    artist: "TJ monterde",
+    title: "Araw Araw",
+    artist: "Ben&Ben",
     coverUrl: "https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-6/495349435_4122846914703048_1886679763937689094_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeH-CdZzzz58ZMKN_g1ZaApPWIRhdg48XVxYhGF2DjxdXPfDVE1U7Wlmzz7xlxZfRSOLlheD4Kv-B5WvEV1KalwT&_nc_ohc=RTaXL0EPwlcQ7kNvwEfdMLF&_nc_oc=AdnAtWWgHKOFo-WmA5--slY6DDUnuRXfG2T1MpH18_E8BAweqHFUdp1Yo2lq3dxs0Kw&_nc_zt=23&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=sbfefZAuUtgEvXjQZRjDHA&oh=00_AfuQa2zJjrbkgaWVvJQzPT_-0VkZw6P9ynbR-_SaXXCAFA&oe=699518BC",
-    audioUrl: "https://audio.jukehost.co.uk/rQRrU4tqsPg7mkin4IeCYL6W2BNhAyBC",
-    note: "Aalagaan ka't di papabayaan,Pagkat ikaw sa'kin ay prinsesa🥰",
+    audioUrl: "https://audio.jukehost.co.uk/mcfXftJkKzhcTHS0Kc1GZStITAbK07XB",
+    note: "pasensya na kung minsan nawawalan ako ng oras sayo pero tandaan mo na nandito ako para sayo",
   },
-  {
-    id: '4',
-    title: "Best Part",
-    artist: "Daniel Caesar",
-    coverUrl: "https://scontent.fmnl14-1.fna.fbcdn.net/v/t39.30808-6/633432471_25719365951019606_252698183004588_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHkp9Nv3N-xInyhAkSTkvDoZQWk0gYNQYxlBaTSBg1BjAAMs-KXy41klXlG-skLp-3twB74GTQXSYlB4iLYWKyM&_nc_ohc=T0aIX8Dz5mAQ7kNvwGP4Fvf&_nc_oc=Admh0JXsTOKNDmKFxQZ5cb8LLEqlfH9b6OaezNBdAGpxycdhTZw-r3D_pvd2ftdADSo&_nc_zt=23&_nc_ht=scontent.fmnl14-1.fna&_nc_gid=g_g5yDb5kjF5a8IIp4um3g&oh=00_AfuQACIK4XKa40E5BN3ZmNr5ew_V947_6ttNQTt5L6ajAQ&oe=699512AC",
-    audioUrl: "https://audio.jukehost.co.uk/oxaRFLeCZmKrHEZW95pgRy5oWEHKWWfS",
-    note: "You don't know, babe, When you hold me, And kiss me slowly☺️,It's the sweetest thing",
-  },
+
 ];
